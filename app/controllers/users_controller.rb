21d1bccb112c10e23bf28e0eea537f8c98228883
George <71434803+george-swift@users.noreply.github.com>
2021-07-31 18:53:29 +0100

    Add Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    if @users
+      render json: { users: @users }
+    else
+      render json: { error: 'No users found' }, status: :internal_server_error
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: { user: @user }
+    else
+      render json: { error: 'User not found' }, status: :internal_server_error
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      render json: { user: @user }, status: :created
+    else
+      render json: { error: @user.error.full_messages }, status: :internal_server_error
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: { message: 'Profile successfully updated' }, status: :ok
+    else
+      render json: { message: 'Could not update profile' }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
