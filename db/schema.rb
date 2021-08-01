7de384b8e490335667778a1caea16b41165bed75
George <71434803+george-swift@users.noreply.github.com>
2021-08-01 18:42:16 +0100

    Generate migration for Lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_105302) do
+ActiveRecord::Schema.define(version: 2021_07_31_180200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_lists_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_07_31_105302) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "lists", "users"
 end
