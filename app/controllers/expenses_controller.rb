91bb130b52ed611a0bb72825570fc36866570cd1
George <71434803+george-swift@users.noreply.github.com>
2021-08-02 13:13:01 +0100

    Add Expenses controller

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,54 @@
+class ExpensesController < ApplicationController
+  before_action :find_list, only: %i[index create]
+  before_action :find_expense, except: %i[index create]
+
+  def index
+    render json: @list.expenses
+  end
+
+  def show
+    render json: @expense
+  end
+
+  def create
+    @expense = @list.expenses.build(expense_params)
+
+    if @expense.save
+      render json: @expense
+    else
+      render json: { error: 'Unable to create expense' }, status: :bad_request
+    end
+  end
+
+  def update
+    if @expense
+      @expense.update(expense_params)
+      render json: { message: 'Expense sucessfully updated' }, status: :ok
+    else
+      render json: { error: 'Unable to update expense' }, status: :bad_request
+    end
+  end
+
+  def destroy
+    if @expense
+      @expense.destroy
+      render json: { message: 'Expense deleted' }, status: :ok
+    else
+      render json: { error: 'Unable to delete expense' }, status: :bad_request
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:id, :title, :amount, :date, :notes, :list_id)
+  end
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+  def find_expense
+    @expense = Expense.find(params[:id])
+  end
+end
