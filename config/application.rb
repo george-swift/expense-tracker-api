354a3d3bed8c2452d50b41fd7ac5437508b1a5f2
George <71434803+george-swift@users.noreply.github.com>
2021-08-09 00:55:13 +0100

    Add origin for resource sharing

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,10 @@ module ExpenseTracker
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_expense_tracker', expire_after: 20.years
+    config.middleware.insert_after(
+      ActionDispatch::Cookies, ActionDispatch::Session::CookieStore, key: '_expense_tracker'
+    )
   end
 end
