c7aa094e8c1f4a789bdb4b5f8494eb6a0fbf0b0c
George <71434803+george-swift@users.noreply.github.com>
2021-08-09 01:28:41 +0100

    Add correct origin to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://redux-expense-tracker.netlify.app/'
+    origins 'https://redux-expense-tracker.netlify.app'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: %i[get post put patch delete options head],
       credentials: true
   end
 end
